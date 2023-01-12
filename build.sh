a9f23d444e35f5e8083c5a9a6a9482cd1bfb8a70
Victor Castell <victor@victorcastell.com>
2023-01-12 17:12:51 +0100

    Fix script

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 # Map of arch and compilers to build with
 declare -A compilers=(
-    ["darwin/arm64"]="o64-clang/o64-clang++"
-    ["darwin/amd64"]="oa64-clang/oa64-clang++"
+    ["darwin/arm64"]="oa64-clang/oa64-clang++"
+    ["darwin/amd64"]="o64-clang/o64-clang++"
     ["linux/arm64"]="aarch64-linux-gnu-gcc/aarch64-linux-gnu-g++"
     ["linux/amd64"]="x86_64-linux-gnu-gcc/x86_64-linux-gnu-g++"
 )
 
 # Loop through compilers list and build
 for arch in "${!compilers[@]}"; do
-  echo "Building for $arch"
-  compiler=$(echo $compilers[$arch] | tr "/")
+  IFS='/' read -ra compiler <<< ${compilers[$arch]}
+  echo "Building for $arch using c ${compiler[0]} and c++ ${compiler[1]}"
   rm mcl/lib/*
-  docker run --entrypoint bash -v `PWD`/mcl:/mcl ghcr.io/goreleaser/goreleaser-cross:v1.19.5 -c "cd /mcl && make -j4 CC=$compiler[0] CPP=$compiler[1]"
+  docker run --entrypoint bash -v `PWD`/mcl:/mcl ghcr.io/goreleaser/goreleaser-cross:v1.19.5 -c "cd /mcl && make -j4 CC=${compiler[0]} CPP=${compiler[1]}"
   mkdir -p mclherumi/lib/$arch
   cp mcl/lib/libmcl.a mclherumi/lib/$arch/libmcl.a
-  cp mcl/lib/libmclbn256.a mclherumi/lib/$arch/libmclbn256.a
+  cp -f mcl/lib/libmclbn256.a mclherumi/lib/$arch/libmclbn256.a
 done
